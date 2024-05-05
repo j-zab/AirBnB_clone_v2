96e952b689753a8b06c3a01e3747fd6cd516608e
job zablon <zablonjob@gmail.com>
2024-05-05 14:14:15 -0700

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,10 +1,52 @@
 #!/usr/bin/python3
-"""script that distributes archive to webservers"""
-from fabric.api import local, hosts
+"""
+A Fabric script to distributes an archive to your web servers
+"""
+
 from datetime import datetime
-from fabric.contrib.files import exists
-hosts_list = ['ubuntu@34.207.190.235', 'ubuntu@100.24.255.166']
+from fabric.api import *
+import os
+
+env.hosts = ['52.207.79.6', '34.234.193.197']
+env.user = "ubuntu"
 
 
-@hosts(hosts_list)
+def do_pack():
+    """
+        returns archive path if archive is generated correctly.
+    """
+
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    archived_f_path = "versions/web_static_{}.tgz".format(date)
+    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
+
+    if t_gzip_archive.succeeded:
+        return archived_f_path
+    else:
+        return None
+
+
 def do_deploy(archive_path):
+    """
+        Distributes archive.
+    """
+    if os.path.exists(archive_path):
+        archived_file = archive_path[9:]
+        newest_version = "/data/web_static/releases/" + archived_file[:-4]
+        archived_file = "/tmp/" + archived_file
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(newest_version))
+        run("sudo tar -xzf {} -C {}/".format(archived_file,
+                                             newest_version))
+        run("sudo rm {}".format(archived_file))
+        run("sudo mv {}/web_static/* {}".format(newest_version,
+                                                newest_version))
+        run("sudo rm -rf {}/web_static".format(newest_version))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(newest_version))
+
+        print("New version deployed!")
+        return True
+
+    return False
